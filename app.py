c8056ca0c9c915ad6ddc4a3371b639c2994f192d
Tran Ngoc Quy <motmaytinh@gmail.com>
2018-08-15 17:29:53 +0700

    Update

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import random
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    adj = open("adj.txt").readlines()
+    return "Ước gì " + adj[random.randint(0, len(adj) - 1)] + " như Đức Linh"
