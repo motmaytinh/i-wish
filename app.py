3889f49599cb51c83cc5bbb48464392cdffa8b26
Tran Ngoc Quy <motmaytinh@gmail.com>
2018-08-16 17:49:47 +0700

    Many update

--- app.py
+++ app.py
@@ -9,25 +9,30 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    try:
-        adj = request.form['text'].upper()
-        choice = request.form['template']
-    except KeyError:
-        adj = ''
-        choice = 'random'
-    print(adj)
-    print(choice)
-    if choice == 'random' and adj == '':
-        adj = open("adj.txt").read().split('\n')
-        make_meme("ƯỚC GÌ " + adj[random.randint(0, len(adj) - 1)], "NHƯ ĐỨC LINH",
-                  "templates/template" + str(random.randint(0, 1)) + ".jpg")
-    elif adj == '':
-        adj = open("adj.txt").read().split('\n')
-        make_meme("ƯỚC GÌ " + adj[random.randint(0, len(adj) - 1)], "NHƯ ĐỨC LINH",
-                  "templates/" + choice)
-    elif choice == 'random':
-        make_meme("ƯỚC GÌ " + adj, "NHƯ ĐỨC LINH",
-                  "templates/template" + str(random.randint(0, 1)) + ".jpg")
+    if request.method == 'POST':
+        try:
+            adj = request.form['text'].upper()
+            choice = request.form['template']
+        except KeyError:
+            adj = ''
+            choice = 'random'
+        print(adj)
+        print(choice)
+        if choice == 'random' and adj == '':
+            adj = open("adj.txt").read().split('\n')
+            make_meme("ƯỚC GÌ " + adj[random.randint(0, len(adj) - 1)], "NHƯ ĐỨC LINH",
+                      "templates/template" + str(random.randint(0, 1)) + ".jpg")
+        elif adj == '':
+            adj = open("adj.txt").read().split('\n')
+            make_meme("ƯỚC GÌ " + adj[random.randint(0, len(adj) - 1)], "NHƯ ĐỨC LINH",
+                      "templates/" + choice)
+        elif choice == 'random':
+            make_meme("ƯỚC GÌ " + adj, "NHƯ ĐỨC LINH",
+                      "templates/template" + str(random.randint(0, 1)) + ".jpg")
+        else:
+            make_meme("ƯỚC GÌ " + adj, "NHƯ ĐỨC LINH", "templates/" + choice)
     else:
-        make_meme("ƯỚC GÌ " + adj, "NHƯ ĐỨC LINH", "templates/" + choice)
+        adj = open("adj.txt").read().split('\n')
+        make_meme("ƯỚC GÌ " + adj[random.randint(0, len(adj) - 1)], "NHƯ ĐỨC LINH",
+                  "templates/template" + str(random.randint(0, 1)) + ".jpg")
     return render_template('meme.html', list=open("adj.txt").read().split('\n'))
