e7bb325313cbbbc85d3096c70b760312c1c0b042
Tran Ngoc Quy <motmaytinh@gmail.com>
2018-08-16 14:31:58 +0700

    Add custom meme

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 from memgenerator import make_meme
 import random
 
@@ -12,7 +13,7 @@ def hello():
     make_meme("ƯỚC GÌ " + adj[random.randint(0, len(adj) - 1)], "NHƯ ĐỨC LINH", "templates/template" + str(random.randint(0, 1)) + ".jpg")
     return render_template('meme.html')
 
-@app.route('/custom')
-def custom_meme(adj):
-    make_meme("ƯỚC GÌ " + adj, "NHƯ ĐỨC LINH", "templates/template" + str(random.randint(0, 1)) + ".jpg")
+@app.route('/custom', methods=['POST', 'GET'])
+def custom_meme():
+    make_meme("ƯỚC GÌ " + request.form['text'].upper(), "NHƯ ĐỨC LINH", "templates/template" + str(random.randint(0, 1)) + ".jpg")
     return render_template('meme.html')
